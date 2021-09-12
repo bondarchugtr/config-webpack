6fce290f9ef46ac0e425ece90623c04bf5e7a746
bondarchugtr <81237327+bondarchugtr@users.noreply.github.com>
2021-09-13 02:11:43 +0300

    add

---
+++ src/index.js
@@ -0,0 +1,27 @@
+class Game {
+    name = 'Violin Charades'
+}
+const myGame = new Game()
+
+// создаем параграф
+const p = document.createElement('p')
+p.textContent = `I like ${myGame.game}.`
+
+// создаем элемент заголовка
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем параграф и заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading, p)
+
+
+
+console.log("Как интересно!")
+const heading = document.createElement('h1')
+heading.textContent = 'Как интересно!'
+
+// добавляем заголовок в DOM
+const root = document.querySelector('#root')
+root.append(heading)
+
---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+
+module.exports = {
+    entry: {
+        main: path.resolve(__dirname, './src/index.js'),
+    },
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'index.bundle.js',
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'webpack Boilerplate',
+            template: path.resolve(__dirname, './src/template.html'), // шаблон
+            filename: 'index.html', // название выходного файла
+        }),
+        new CleanWebpackPlugin(),
+
+    ],
+    module: {
+        rules: [
+            // JavaScript
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: ['babel-loader'],
+            },
+        ],
+    }
+}
